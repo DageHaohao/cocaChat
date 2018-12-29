0b27d513a8c5d2b86f2c818150f6724c55a476cb
huangjunhao <>
2018-12-29 14:31:29 +0800

    完成图片选择器封装
    未解决的问题：动态权限申请

--- app/src/main/java/hjh/cocachat/frags/main/ActiveFragment.java
+++ app/src/main/java/hjh/cocachat/frags/main/ActiveFragment.java
@@ -1,11 +1,16 @@
 package hjh.cocachat.frags.main;
 
 
+import butterknife.BindView;
 import hjh.cocachat.R;
 import hjh.common.app.Fragment;
+import hjh.common.widget.GalleyView;
 
 
 public class ActiveFragment extends Fragment {
+
+    @BindView(R.id.galleyView)
+    GalleyView mGalley;
 
 
     public ActiveFragment() {
@@ -13,10 +18,22 @@ public class ActiveFragment extends Fragment {
     }
 
 
-
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_active;
     }
 
+
+    /*@Override
+    protected void initData() {
+        super.initData();
+
+        mGalley.setup(getLoaderManager(), new GalleyView.SelectedChangeListener() {
+            @Override
+            public void onSelectedCountChanged(int count) {
+
+            }
+        });
+
+    }*/
 }
